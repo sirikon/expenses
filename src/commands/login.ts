e95d5444f04cbe56963a953b2d41ba54c654c258
Carlos Fdez. Llamas <hello@sirikon.me>
2021-05-02 16:17:28 +0200

    Use zenity for login

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -9,20 +9,36 @@ export default async function loginCommand() {
 }
 
 async function askCredentials(): Promise<Credentials> {
+    const process = Deno.run({
+        cmd: ['zenity', '--password', '--username', '--title=BBVA Credentials'],
+        stdout: 'piped'
+    })
+    const processStatus = await process.status();
+    if (processStatus.code > 0) {
+        throw new Error(`Asking credentials failed with exit code ${processStatus.code}`);
+    }
+    const [username, password] = (new TextDecoder().decode(await process.output())).trim().split('|');
     return {
-        username: await ask('Username'),
-        password: await ask('Password')
+        username,
+        password
     }
 }
 
-async function ask(description: string): Promise<string> {
-    Deno.stdout.write(new TextEncoder().encode(`${description}: `));
-    for await (const line of readLines(Deno.stdin)) {
-        if (line === '') continue;
-        return line;
-    }
-    return '';
-}
+// async function askCredentials(): Promise<Credentials> {
+//     return {
+//         username: await ask('Username'),
+//         password: await ask('Password')
+//     }
+// }
+
+// async function ask(description: string): Promise<string> {
+//     Deno.stdout.write(new TextEncoder().encode(`${description}: `));
+//     for await (const line of readLines(Deno.stdin)) {
+//         if (line === '') continue;
+//         return line;
+//     }
+//     return '';
+// }
 
 async function setFileContent(path: string, content: string): Promise<void> {
     const encoder = new TextEncoder();
