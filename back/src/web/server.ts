698b87059ff262721b48fcae2d4f95e358a9732e
Carlos Fdez. Llamas <hello@sirikon.me>
2021-10-24 16:46:13 +0200

    on listen

--- back/src/web/server.ts
+++ back/src/web/server.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "oak/mod.ts";
+import { ApplicationListenEvent } from "oak/application.ts";
 import { ExApp, ExRouter } from "./models.ts";
 import routes from "./routes.ts";
 
@@ -10,6 +11,10 @@ export default async () => {
   app.use(router.routes());
   app.use(router.allowedMethods());
 
-  console.log("Listening http://127.0.0.1:8000");
-  await app.listen({ port: 8000 });
+  app.addEventListener("listen", onListen);
+  await app.listen({ hostname: "0.0.0.0", port: 8000 });
 };
+
+function onListen(e: ApplicationListenEvent) {
+  console.log(`Listening on http://127.0.0.1:${e.port}`);
+}
