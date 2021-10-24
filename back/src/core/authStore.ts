b783c682d2d507363bf3109625bc8875fe7800eb
Carlos Fdez. Llamas <hello@sirikon.me>
2021-10-24 16:11:15 +0200

    tweak

--- back/src/core/authStore.ts
+++ back/src/core/authStore.ts
@@ -1,7 +1,7 @@
-import { join, dirname } from 'std/path/mod.ts'
+import { join as joinPath, dirname } from 'std/path/mod.ts'
 
 export async function save(sourceId: string, auth: unknown) {
-  const path = join('credentials', `${sourceId}.json`)
+  const path = joinPath('credentials', `${sourceId}.json`)
 
   const encoder = new TextEncoder();
   const data = encoder.encode(JSON.stringify(auth, null, 2));
